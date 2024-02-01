0f2a89bfc253297caf602352d4b8ea98ad4a216e
Patrick Alvin Alcala <45809622+PatAlcala9@users.noreply.github.com>
2024-02-01 19:08:38 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 import compressor from "astro-compressor";
+// import { astroImageTools } from "astro-imagetools";
 
 // https://astro.build/config
 export default defineConfig({
