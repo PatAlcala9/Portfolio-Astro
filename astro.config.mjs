7974207f58a791b2c182659d165a8adf72f0e8fd
PatCode9 <patrick_alv@yahoo.com>
2024-02-06 11:24:34 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
 import robotsTxt from 'astro-robots-txt';
-
 import compressor from "astro-compressor";
-// import { astroImageTools } from "astro-imagetools";
 
 // https://astro.build/config
 export default defineConfig({
