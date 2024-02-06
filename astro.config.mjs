059560acb0cd84dfaa7466970a982effb43eabb7
PatCode9 <patrick_alv@yahoo.com>
2024-02-06 11:07:23 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
+import robotsTxt from 'astro-robots-txt';
 
 import compressor from "astro-compressor";
 // import { astroImageTools } from "astro-imagetools";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [compressor({gzip: false, brotli: true})]
+  site: 'https://patalcala.com',
+  
+  integrations: [compressor({gzip: false, brotli: true}), robotsTxt()]
 });
