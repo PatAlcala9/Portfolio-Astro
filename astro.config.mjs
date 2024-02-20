32159055d592a5527419e59acdcca7e6c5f9acd1
Patrick Alvin Alcala <45809622+PatAlcala9@users.noreply.github.com>
2024-02-21 01:18:35 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,6 @@ export default defineConfig({
   site: 'https://patalcala.com',
   
   integrations: [compressor({gzip: false, brotli: true}), robotsTxt()],
-  compressHTML: false,
   build: {
     assets: '_patalcala',
     // inlineStylesheets: `always`,
