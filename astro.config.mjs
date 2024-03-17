dab1d19285e6ee14e8437d09e7404d044baa8671
Patrick Alvin Alcala <45809622+PatAlcala9@users.noreply.github.com>
2024-03-17 18:04:28 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import compressor from "astro-compressor";
 // import { resolve } from 'node:path'
 import { shield } from '@kindspells/astro-shield';
 const rootDir = new URL('.', import.meta.url).pathname;
+import min from 'astro-min'
 
 // https://astro.build/config
 export default defineConfig({
@@ -14,6 +15,22 @@ export default defineConfig({
       enableStatic_SRI: true, // true by default
       enableMiddleware_SRI: false, // false by default
       // sriHashesModule: resolve(rootDir, 'src', 'utils', 'sriHashes.mjs'),
+    }),
+  minify({
+      do_not_minify_doctype: false,
+      ensure_spec_compliant_unquoted_attribute_values: false,
+      keep_closing_tags: false,
+      keep_comments: false,
+      keep_html_and_head_opening_tags: false,
+      keep_input_type_text_attr: false,
+      keep_spaces_between_attributes: false,
+      keep_ssi_comments: false,
+      minify_css: false,
+      minify_js: false,
+      preserve_brace_template_syntax: false,
+      preserve_chevron_percent_template_syntax: false,
+      remove_bangs: false,
+      remove_processing_instructions: false,
     })],
   build: {
     assets: '_patalcala',
