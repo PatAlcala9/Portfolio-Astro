6a9f4bf7f19ff39b0b8c77c70ee2f533b954e8bf
Patrick Alvin Alcala <patrick_alv@yahoo.com>
2025-05-21 11:12:16 +0800

    added analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,17 +4,17 @@ import compressor from "astro-compressor";
 // import { resolve } from 'node:path'
 import { shield } from '@kindspells/astro-shield';
 
+import partytown from '@astrojs/partytown';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://patalcala.com',
   
-  integrations: [compressor({gzip: false, brotli: true}), robotsTxt(),
-  shield({
+  integrations: [compressor({gzip: false, brotli: true}), robotsTxt(), shield({
       enableStatic_SRI: true, // true by default
       enableMiddleware_SRI: false, // false by default
       // sriHashesModule: resolve(rootDir, 'src', 'utils', 'sriHashes.mjs'),
-    }),
-  ],
+    }), partytown()],
   build: {
     assets: '_patalcala',
     // inlineStylesheets: `always`,
